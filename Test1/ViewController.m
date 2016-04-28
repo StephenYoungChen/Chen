290f89d36bd177b84b86cb6cb8d87e07b3004b99
yzz <810178955@qq.com>
2016-04-28 11:14:01 +0800

    -分支上面的操作

--- Test1/ViewController.m
+++ Test1/ViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor greenColor];
+    UILabel * label = [[UILabel alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    label.backgroundColor = [UIColor redColor];
+    [self.view addSubview:label];
 
 }
 
